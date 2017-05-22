fa88bf7726f07b8d280ed0dbb2659aa6832f4db1
jmiths <jacobwilly@gmail.com>
2017-05-21 23:44:29 -0400

    fixed put to post for update

--- API/test/test_update.js
+++ API/test/test_update.js
@@ -6,10 +6,10 @@ var should = chai.should();
 chai.use(chaiHttp);
 
 describe("Test API related with update", () => {
-    describe("/PUT update location with wrong access token", () => {
+    describe("/POST update location with wrong access token", () => {
         it("it should GET 404 status", (done) => {
             chai.request(server)
-                .put("/update/location?accesstoken=this_is_not_test_access_token")
+                .post("/update/location?accesstoken=this_is_not_test_access_token")
                 .send({lat: "123", lng: "456"})
                 .end((err, res) => {
                     res.should.have.status(404);
@@ -18,10 +18,10 @@ describe("Test API related with update", () => {
                 });
         });
     });
-    describe("/PUT update location with right access token", () => {
+    describe("/POST update location with right access token", () => {
         it("it should GET 200 status", (done) => {
             chai.request(server)
-                .put("/update/location?accesstoken=this_is_test_access_token")
+                .post("/update/location?accesstoken=this_is_test_access_token")
                 .send({lat: "123", lng: "456"})
                 .end((err, res) => {
                     res.should.have.status(200);
